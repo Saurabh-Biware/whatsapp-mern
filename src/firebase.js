12e9a59c035ae82dfafc5b1bb40e3c4f0c097659
Saurabh Biware <saurabhbiware@Saurabhs-MacBook-Air.local>
2020-12-29 11:42:34 +0100

    Initialized firebase and started creating the components

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDJuoRDFp5H4OXn2CwX5r0Kqm-gkQ0z3Os",
+    authDomain: "whatsapp-mern-35594.firebaseapp.com",
+    projectId: "whatsapp-mern-35594",
+    storageBucket: "whatsapp-mern-35594.appspot.com",
+    messagingSenderId: "631143735883",
+    appId: "1:631143735883:web:4557579aff35f5429ce3ce"
+  };
